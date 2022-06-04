78cf963e725e2818b26754c76b58f55cbce12b6f
Trixall <54065782+TrixallTR@users.noreply.github.com>
2022-06-04 16:36:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import os
 from os import makedirs
 
 # https://game-assets.brawlstarsgame.com
@@ -20,18 +21,22 @@ def Download(file, url):
         file = file["file"]
         request = requests.get(f"{url}/{sha}/{file}")
 
-        folder = file.split("/", 1)[0]
+        if request.status_code == 200:
+ 
+            folder = os.path.split(file)[0]
 
-        if file.split("/", 2)[1] == "background":
-            makedirs(f"{sha}/{folder}/background", exist_ok=True)
+            makedirs(f"{sha}/{folder}", exist_ok = True)
 
-        makedirs(f"{sha}/{folder}", exist_ok=True)
+            f = open(f"{sha}/{file}", "wb")
+            f.write(request.content)
+            f.close
 
-        f = open(f"{sha}/{file}", "wb")
-        f.write(request.content)
-        f.close
-
-        print(f"Downloaded {file}")
+            print(f"Downloaded {file}")
+        
+        else:
+            
+            print(f"{file} Status Code: {request.status_code}")
+            break
 
 file = input("Fingerprint File:\n")
 url = input("Assets URL:\n")
